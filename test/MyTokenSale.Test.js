effee05415fa3dea83c301ff93bd24e0a3298537
Aryan Shah <05aryanshah@gmail.com>
2022-05-24 01:57:09 +0530

    Adding Tests to the Sale Contract

--- test/MyTokenSale.Test.js
+++ test/MyTokenSale.Test.js
@@ -13,9 +13,17 @@ contract("Token Test", function(accounts) {
         return expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0));
     });
     it('all tokens should be present in the MyTokenSale Contract', async() => {
-
+        let instance = await MyToken.deployed();
+        let balanceOfSaleSC = await instance.balanceOf(MyTokenSale.address);
+        let totalSupply = await instance.totalSupply();
+        expect(balanceOfSaleSC).to.be.bignumber.equal(totalSupply);
     });
     it('should be possible to buy tokens',async() =>{
+        let tokeninstance = await MyToken.deployed();
+        let saleInstance = await MyTokenSale.deployed();
+        let beforeBalance = await tokeninstance.balanceOf(deployerAccount);        
+        await expect(saleInstance.sendTransaction({from: deployerAccount , value: web3.utils.toWei("1", "wei")})).to.be.fulfilled;
+        return expect(tokeninstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(beforeBalance.add(new BN(1)));
 
     });
 });
